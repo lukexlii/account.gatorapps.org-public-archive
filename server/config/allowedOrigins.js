ab733bf6d9997449fc13a7d73bd4481f5bb0d958
Luke Li <luke@lukexli.com>
2023-07-01 14:19:05 +0800

    Backend: Fixed CORS config

---
+++ server/config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'https://account.ufapps.org',
+    'http://localhost:8000',
+    'http://localhost:3000'
+];
+
+module.exports = allowedOrigins;
--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,11 +1,12 @@
+const allowedOrigins = require('./allowedOrigins');
+
 const corsOptions = {
     origin: (origin, callback) => {
-        callback(null, true)
-        /* if (process.env.ALLOWED_ORIGINS.indexOf(origin) !== -1 || !origin) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
-        } */
+        }
     },
     optionsSuccessStatus: 200
 }
