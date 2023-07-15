f91e4dc38708e45d0e03e2b58999b6110d779d3a
Luke Li <luke@lukexli.com>
2023-07-15 23:58:53 +0800

    Frontend: button hover color fixed

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -50,7 +50,10 @@ class Login extends React.Component {
                     'font-size': '1.375rem',
                     'font-weight': '700',
                     'text-transform': 'none',
-                    'background-color': 'rgb(40, 87, 151)'
+                    'background-color': 'rgb(40, 87, 151)',
+                    ':hover': {
+                      'background-color': 'rgb(17, 82, 147)'
+                    }
                   }
                 }>
                   <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 43.5 29.4' height='36' width='36' fill='white' icon='logo-uf'><path d='M31.1 24.2v-7.5h6.8v-4.9h-6.8V4.9h7.5v2.7h4.9V0H23.7v4.9h1.8v19.3h-1.8v4.9h9.1v-4.9h-1.7zM21.1 18.1V4.9h1.8V0h-9.2v4.9h1.8v11.6c0 4.9-.6 7.2-4 7.2s-4-2.3-4-7.2V4.9h1.8V0H0v4.9h1.8v13.2c0 2.9 0 5.3 1.4 7.4 1.5 2.4 4.3 3.9 8.3 3.9 7.1 0 9.6-3.7 9.6-11.3z'></path></svg>
