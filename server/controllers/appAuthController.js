f27e4cccaf970840d86306d3361e35cd39e1742c
Luke Li <luke@lukexli.com>
2023-07-30 02:00:30 +0800

    Backend: Updated DB connection in previous commit; minor jwt fix in appauth

--- server/controllers/appAuthController.js
+++ server/controllers/appAuthController.js
@@ -34,8 +34,8 @@ const validateRequest = async (req, res) => {
       appAuthPublicKey,
       { algorithms: ['ES256'] },
       (err, decoded) => {
-        if (err.name === 'JsonWebTokenError') return res.status(400).json({ 'errCode': '-200203', 'errMsg': 'Your authentication state is malformed, please examine your request and try again' });
-        if (err.name === 'TokenExpiredError') return res.status(400).json({ 'errCode': '-200204', 'errMsg': 'Your authentication request has expired, please re-initiate a request' });
+        if (err?.name === 'JsonWebTokenError') return res.status(400).json({ 'errCode': '-200203', 'errMsg': 'Your authentication state is malformed, please examine your request and try again' });
+        if (err?.name === 'TokenExpiredError') return res.status(400).json({ 'errCode': '-200204', 'errMsg': 'Your authentication request has expired, please re-initiate a request' });
         if (err) return res.status(400).json({ 'errCode': '-200205', 'errMsg': "We're sorry, but we are unable to validate your authentication state, possibly because the state is malformed, please re-initiate a request or try again later" });
         return res.status(200).json({ 'errCode': '0', 'serviceDisplayName': foundService.displayName });
       }
